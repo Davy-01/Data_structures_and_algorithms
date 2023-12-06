192780ee8b2f8e3b52445d5a9b9b06d6c1a98975
Davis <daviesnyakiba@gmail.com>
2023-12-06 15:26:23 +0300

    If statement to checking frequency of word in a sentence

--- dictionaries.py
+++ dictionaries.py
@@ -8,3 +8,11 @@ def word_frequency(sentence):
     words = sentence.split()
 
     frequency_dict = {}
+
+    for word in words:
+        if word in frequency_dict:
+            frequency_dict[word] += 1
+        else:
+            frequency_dict[word] = 1
+
+    return frequency_dict
